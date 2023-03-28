31e7b8c621f6a426a27a602925d9cc466e2c2546
Achref LAABIDI <abidi.achref95@gmail.com>
2023-03-28 20:33:18 +0100

    stopped at iteration 3

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,19 +1,43 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 === num2) {
+    return num1
+  } else if (num1 > num2) {
+    return num1
+  } else {
+    return num2
+  }
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+  if (words.length === 0) {
+    return null;
+  }
+
+  let longestWord = words[0]
+  for (let i = 1; i < words.length; i++) {
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i]
+    }
+  }
+  return longestWord
+}
 
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+  return numbers.reduce(function(sum, current) {
+    return sum + current
+  }, 0 // 0 is the initial "sum" + 6 is the current value)
+}
 
 
 
